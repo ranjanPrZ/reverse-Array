2977646a782372bc80699e30be2581f5a386710f
ranjanPrZ <35763477+ranjanPrZ@users.noreply.github.com>
2020-06-28 14:48:32 +0530

    Update binary_searc.rb

--- binary_searc.rb
+++ binary_searc.rb
@@ -1,10 +1,8 @@
 def binary_search (a,key)  # a is the array and key is the value to be found
     lo = 0
     hi= a.length-1
-    
     while (lo<=hi)
         mid = lo+((hi-lo)/2)
-        
         if a[mid] == key
             return mid
         elsif a[mid] < key
@@ -12,10 +10,8 @@ def binary_search (a,key)  # a is the array and key is the value to be found
         else
             hi=mid-1
         end
-        
-    end
-    
-    return "Value not found in array"
+    en
+    returnd "Value not found in array"
 end
 
 p binary_search([22,12,3,14,55,12], 3)
