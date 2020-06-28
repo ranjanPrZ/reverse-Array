ee216861e3fb704db2f4af9e53eb95972da02502
RANJAN KUMAR <ranjantabby@gmail.com>
2020-06-28 14:42:32 +0530

    Basic program to find number position in array * Using binary search find given number position

---
+++ binary_searc.rb
@@ -0,0 +1,22 @@
+def binary_search (a,key)  # a is the array and key is the value to be found
+    lo = 0
+    hi= a.length-1
+    
+    while (lo<=hi)
+        mid = lo+((hi-lo)/2)
+        
+        if a[mid] == key
+            return mid
+        elsif a[mid] < key
+            lo=mid+1
+        else
+            hi=mid-1
+        end
+        
+    end
+    
+    return "Value not found in array"
+end
+
+p binary_search([22,12,3,14,55,12], 3)
+#=> 2
